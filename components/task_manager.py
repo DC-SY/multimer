fa8e4b24ca2de80c4ca0c88e631f969ff37c1fcd
Xiao_DC <3284155190@qq.com>
2024-10-30 10:35:51 +0800

    完成任务查询和展示

--- components/task_manager.py
+++ components/task_manager.py
@@ -7,10 +7,10 @@ def add_task(task_name):
         cursor.execute("INSERT INTO tasks (task_name) VALUES (?)", (task_name,))
         conn.commit()
 
-def get_tasks():
+def get_display_tasks():
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM tasks WHERE deleted_at IS NULL")
+        cursor.execute("SELECT task_name, start_time, end_time, duration, created_at FROM tasks WHERE deleted_at IS NULL")
         return cursor.fetchall()
 
 # 可扩展其他函数：update_task, delete_task等
--- pages/task_page.py
+++ pages/task_page.py
@@ -1,5 +1,11 @@
-import streamlit as st
+import sqlite3
 
-def show():
-    st.title("任务管理")
-    st.write("这里是任务管理页面")
+import pandas as pd
+import streamlit as st
+from components.task_manager import *
+
+# 获取数据
+task_data = get_display_tasks()
+# 转换为DataFrame
+df = pd.DataFrame(task_data, columns=["任务名称", "开始时间", "结束时间", "持续时间", "创建时间"])
+st.dataframe(df)
